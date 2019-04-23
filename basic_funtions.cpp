9a830b56ec4fcd6ebb95998ea41057565da76ed0
SpayL <43752455+SpayL@users.noreply.github.com>
2019-04-23 10:23:25 +0530

    payal

--- basic_funtions.cpp
+++ basic_funtions.cpp
@@ -17,6 +17,27 @@ class odd_number
 	}
 	return -1;
 }
+class Node  {  
+    public: 
+    int data;  
+    Node* next;  }; 
+
+  
+void deleteList(Node** head_ref)  
+{ Node* current = *head_ref;  
+Node* next;
+while (current != NULL)  {  
+    next = current->next;  
+    free(current);  
+    current = next;  }  
+*head_ref = NULL;  }  
+ 
+void push(Node** head_ref, int new_data)  {  
+    Node* new_node = new Node(); 
+     new_node->data = new_data;  
+      new_node->next = (*head_ref);  
+      (*head_ref) = new_node;  }  
+
 
 class missing_number
 {
